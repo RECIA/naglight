803a1eeeb02e82790add65e3da044dc063afbe30
Marc Lagrange <rhaamo@gruik.at>
2010-10-30 16:41:20 +0200

    Add allhosts API and View

--- naglight.rb
+++ naglight.rb
@@ -10,15 +10,24 @@ $mk_livestatus_socket_paths += ["/home/nagios/live"]
 # true / false
 $mk_livestatus_debug=true
 
+def get_mk(query)
+  mk_array_to_hash(JSON.parse(get_mk_livestatus(query)))
+end
 
 get '/' do
   @title = "Index"
   haml :index
 end
 
+get '/allhosts' do
+  @title = "All Hosts"
+  @allhosts = get_mk({:table => "hosts"})
+  haml :"hosts/allhosts"
+end
+
 get '/contacts' do
   @title = "Contacts"
-  @contacts = mk_array_to_hash(JSON.parse(get_mk_livestatus({:table => "contacts"})))
+  @contacts = get_mk({:table => "contacts"})
   haml :"other/contacts"
 end
 
@@ -28,6 +37,11 @@ end
 
 get '/api/?' do
   haml :"api/index"
+end
+
+get '/api/get/allhosts' do
+  response.header['Content-type'] = 'application/x-javascript; charset=UTF-8'
+  return get_mk_livestatus({:table => "hosts"})
 end
 
 get '/api/get/contacts' do
