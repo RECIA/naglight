9c191bb4d2e8332dc456929d408ede519cdda3ff
Marc Lagrange <rhaamo@gruik.at>
2010-11-01 11:33:50 +0100

    Remove non working cycle method

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -47,24 +47,3 @@ end
 def host_get_services(query)
   return get_services(query)
 end
-
-
-
-
-def cycle(first_value, *values)
-  values.unshift(first_value)
-  return Cycle.new(*values)
-end
-
-class Cycle
-  def initialize(first_value, *values)
-    @values = values.unshift(first_value)
-    @index = 0
-  end
-  
-  def to_s
-    value = @values[@index].to_s
-    @index = (@index + 1) % @values.size
-    return value
-  end
-end
